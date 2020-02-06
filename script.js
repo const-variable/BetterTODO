ac36eec667754d2b5496d22ee0305a1c2915fbd2
Maulikkumar Chaudhary <maulikkumarchaudhary@Maulik-Tekion-MacBookPro.local>
2020-02-06 11:59:05 +0530

    Initial commit with html done

---
+++ script.js
@@ -0,0 +1,18 @@
+let lists = {
+        list1: [
+            {id: 1, text: 'Drink some water', completed: true},
+            {id: 2, text: 'Go read javascript', completed: false},
+            {id: 3, text: 'Complete ToDo app', completed: false},
+        ],
+        list2: [
+            {id: 1, text: 'Eat 2 bananas today', completed:false },
+            {id: 2, text: 'Plan this weekend', completed: false}
+        ]
+}
+
+
+class List {
+    constructor(){
+        
+    }
+}
